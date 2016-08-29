5a2701e336538b6bfc5fc77b731a28524d565d2d
Wim Bonthuis <wim@500k.nl>
2016-08-29 20:03:51 +0200

    Added vps role.

---
+++ manifests/vps.pp
@@ -0,0 +1,8 @@
+class roles::vps {
+
+  class { '::profiles::lamp': } ->
+  class { '::profiles::mailserverbackend': } ->
+  class { '::profiles::postfixadmin': } ->
+  class { '::profiles::mailserverfrontend': }
+
+}
